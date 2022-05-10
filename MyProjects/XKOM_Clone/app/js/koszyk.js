87ad161d288c2c4d6fe26a666a01ed2ea74b6702
pjoterini <piotrekjan.gorski@gmail.com>
2022-05-10 17:50:04 +0200

    js

---
+++ MyProjects/XKOM_Clone/app/js/koszyk.js
@@ -0,0 +1,10 @@
+// ADD TO CART BTN
+
+const addToCartBtn = document.querySelector('.add-to-cart-btn')
+const itemQuantityCart = document.querySelector('.item-quantity-cart')
+
+addToCartBtn.addEventListener('click', () => {
+    let itemQuantityNumber = 0 
+    itemQuantityNumber++
+    console.log(itemQuantityNumber) 
+})
